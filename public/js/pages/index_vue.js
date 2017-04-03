46bd753d561f8ffd36c205de70b6e24e7bfd95da
Loek <Loek@loek-laptop>
2017-04-03 13:34:52 +0200

    Implemented search and edit functionality for profile pages. Athentication has yet to be implemented

--- public/js/pages/index_vue.js
+++ public/js/pages/index_vue.js
@@ -5,17 +5,19 @@ new Vue({
         j_loginPassword: '',
         j_loginUserName: '',
         activeUser: {},
+        authenticatedUser: {},
         posts: [],
         searchedPosts: [],
         viewtype: "index",
         imageURL: '',
         followers: [],
+        errorMessage: '',
         following: [],
         searchTerm: '',
         subview: "tweets",
         loggedOn: false,
-        biographyChars: 0,
-        postChars: 0,
+        biographyChars: 160,
+        postChars: 160,
         newUserName: '',
         newPostTitle: '',
         newPostContent: ''
@@ -23,17 +25,32 @@ new Vue({
     mounted: function ()
     {
         this.getUsers();
+        Vue.http.options.emulateJSON = true;
     },
     methods:
             {
+                clearChars() {
+                    this.biographyChars = 160;
+                    this.postChars = 160;
+                },
+                displayHome: function () {
+                    this.viewtype = 'index';
+                },
                 displayUser: function (user) {
-                    this.viewtype = "user";
-                    this.activeUser = user;
-                    this.newUserName = user.userName;
-                    this.imageURL = "../images/" + user.profilePicture;
-                    this.getUserPosts();
-                    this.getFollowers();
-                    this.getFollowing();
+                    if (user.userName === this.authenticatedUser.userName) {
+                        this.renderProfilePage();
+                        this.activeUser = user;
+                        this.newUserName = user.userName;
+                        this.imageURL = "../images/" + user.profilePicture;
+                    } else {
+                        this.viewtype = "user";
+                        this.activeUser = user;
+                        this.newUserName = user.userName;
+                        this.imageURL = "../images/" + user.profilePicture;
+                        this.getUserPosts();
+                        this.getFollowers();
+                        this.getFollowing();
+                    }
                 },
                 displayModerator: function (user) {
                     this.viewtype = "moderator";
@@ -43,6 +60,9 @@ new Vue({
                 },
                 renderProfilePage: function () {
                     this.viewtype = 'profile';
+                    this.getUserPosts();
+                    this.getFollowers();
+                    this.getFollowing();
                     this.getAllConnectedPosts();
                     this.searchPosts();
                 },
@@ -51,30 +71,29 @@ new Vue({
                         console.log("HttpGet Request called on url : " + url + " Response:", response.body);
                         this[property] = response.body;
                     }, response => {
-                        // error callback 
+// error callback 
                     });
                 },
                 doHttpPost(url, data, callback) {
-
                     var context = this;
-            console.log("doHttpPost" , callback);
+                    console.log("doHttpPost", callback);
                     Vue.http.headers.common['Authorization'] = 'Basic Og==';
                     this.$http.post(url, data, {
                         headers: {"Content-Type": 'application/x-www-form-urlencoded'}}).then(response => {
                         console.log("HttpPost Request called on url : " + url + " Response:", response.body, data);
                         console.log(response.body);
                         console.log(response);
-                        if (response.body === 'true post') {
+                        if (response.body !== 'false') {
                             console.log("detected True:", response.body);
                             if (callback !== undefined) {
                                 console.log("callback not undefined");
-                                callback(context);
+                                callback(context, response.body);
                             }
                         } else {
                             console.log("detected false:", response.body);
                         }
                     }, response => {
-                        console.log('postFailed');
+                        context.errorMessage = 'Incorrect username or password, please try again';
                     });
                 },
                 changeSubview(type) {
@@ -113,17 +132,23 @@ new Vue({
                 },
                 removeUser(user) {
                     this.$http.get('http://localhost:8080/Kwetter/resources/api/user/remove/' + user.userName, headers = "Access-Control-Allow-Origin").then(response => {
-                        let index = this.users.indexOf(user)
-                        this.users.splice(index, 1);
+                        var context = this;
+                        if (user.userName !== context.authenticatedUser.userName) {
+                            let index = this.users.indexOf(user);
+                            context.users.splice(index, 1);
+                        } else{
+                            context.errorMessage = 'You cannot remove yourself';
+                        }
+
                     }, response => {
-                        // error callback 
+// error callback 
                     });
                 },
                 getAllConnectedPosts() {
                     this.doHttpGet('http://localhost:8080/Kwetter/resources/api/user/findAllConnectedPosts/' + this.activeUser.userName, 'posts');
                 },
                 editUser() {
-                    Vue.http.options.emulateJSON = true;
+
                     var formData = new FormData();
                     formData.append('user', this.activeUser);
                     this.doHttpPost('http://localhost:8080/Kwetter/resources/api/user/edit', this.activeUser);
@@ -133,26 +158,52 @@ new Vue({
                         let index = this.posts.indexOf(post)
                         this.posts.splice(index, 1);
                     }, response => {
-                        // error callback 
+// error callback 
                     });
                 },
+                logout() {
+                    this.authenticatedUser = {};
+                    this.displayUser(this.activeUser);
+                },
                 authenticateUser() {
-                    this.doHttpPost('http://localhost:8080/Kwetter/j_security_check', {"j_username": this.j_loginUserName, "j_password": this.j_loginPassword});
+                    Vue.http.options.emulateJSON = true;
+                    this.doHttpPost('http://localhost:8080/Kwetter/resources/api/user/authenticate', {"username": this.j_loginUserName, "password": this.j_loginPassword}, function (context, user) {
+                        console.log("User : ", user);
+                        var subContext = context;
+                        if (user['role'] === "Moderator") {
+                            console.log("Moderator detected ");
+                            console.log(context);
+                            subContext.viewtype = "moderator";
+                        } else {
+                            context.renderProfilePage();
+                        }
+                        context.activeUser = user;
+                        context.newUserName = user.userName;
+                        context.authenticatedUser = user;
+                        context.getUserPosts();
+                        context.getAllConnectedPosts();
+                        context.imageURL = "../images/" + user.profilePicture;
+
+                        context.j_loginUserName = '';
+                        context.j_loginPassword = '';
+                    });
                 },
                 sendPost() {
                     Vue.http.options.emulateJSON = true;
-                    var formData = new FormData();
-                    formData.append('username', this.activeUser.userName);
                     if (this.doHttpPost('http://localhost:8080/Kwetter/resources/api/user/post/', {"username": this.activeUser.userName, 'content': this.newPostContent, 'title': this.newPostTitle}, function (context) {
                         context.getAllConnectedPosts();
+                        context.searchPosts();
                         context.searchTerm = '';
-                        context.searchPosts(); 
-                    })); 
-            },
+                        context.newPostTitle = '';
+                        context.newPostContent = '';
+                        context.clearChars();
+                    }))
+                        ;
+                },
                 searchPosts() {
                     var context = this;
                     context.searchedPosts = [],
-                            console.log("searchPosts triggered!");
+                            console.log("searchPosts triggered!", this.searchTerm);
                     for (var i = 0; i < this.posts.length; i++) {
                         let index = this.posts[i].content.indexOf(this.searchTerm);
                         if (index >= 0) {
